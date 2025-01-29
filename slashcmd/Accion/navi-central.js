56a17f06920498c506f2cadd8d3cd2ebc8584e83
Angel M-V <angel.m.v.2005@gmail.com>
2025-01-29 00:04:06 -0600

    Actualizacion de archivos

---
+++ slashcmd/Accion/navi-central.js
@@ -0,0 +1,45 @@
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, ChatInputCommandInteraction, ApplicationCommandOptionType} = require(`discord.js`)
+const {SlashCommandBuilder} = require("@discordjs/builders")
+const subcommands = {
+    dance: require("../../handlers/CMDHandler/Interactions/DanceInteract")
+}
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("act")
+    .setDescription("Haz una acción.")
+    .setContexts(['Guild'])
+    .addSubcommand((sub) => 
+        sub.setName("dance")
+        .setDescription("[💃] Mueve el cuerpo")
+        .addUserOption(user => 
+            user.setName("usuario")
+            .setDescription("Selecciona a tu acompañante (Usuario)")
+            .setRequired(false)
+        )
+        .addIntegerOption(character => 
+            character.setName("personaje")
+            .setDescription("ingresa la ID del Alumno [Personaje]")
+            .setMaxValue(1000)
+            .setMinValue(1)
+            .setAutocomplete(true)
+        )
+),
+    deleted: false,
+
+    /**
+     * 
+     * @param {*} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+
+ejecutar: async(client, interaction) => {
+    const subcommand = interaction.options.getSubcommand()
+
+    if(subcommand === "dance") {
+        subcommands.dance(client, interaction)
+    }
+}
+    
+}
